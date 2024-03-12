3d98616dfa2dff3dcf53eed4af9639fcd032e17c
SantiagoNunezSelem <gigogisantisd@gmail.com>
2024-03-12 01:25:12 -0300

    Chess board added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import ChessBoard from "./components/ChessBoard.js";
 
 function App() {
   return (
     <div className="App">
       Hello world
+      <ChessBoard></ChessBoard>
     </div>
   );
 }

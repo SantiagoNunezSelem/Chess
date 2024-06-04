117a7486b2d75c358d1f97f2d022e3e566a409db
Santiago N.S. <gigogisantisd@gmail.com>
2024-06-03 23:47:18 -0300

    Update ChessSquare.js

--- src/components/ChessSquare.js
+++ src/components/ChessSquare.js
@@ -1,13 +1,38 @@
 import React,{useState} from "react"
 import "../stylesheets/ChessSquare.css";
 
-function ChessSquare( {x,y,pieceName,pieceColor,pieceImg,legalPieceMoves} ){
+function ChessSquare( {x,y,pieceName,pieceColor,pieceImg,clickCoordinates,infoSquareSelected,infoPossiblesMoves} ){
 
-    const [legalMoves,setLegalMoves] = useState([])
+    //Tal vez me convenga gestionarlo con javascript
+    let squareSelected = ""
+    if(pieceName !== "" && infoSquareSelected.x === x && infoSquareSelected.y === y)
+        squareSelected = "square-selected"
+
+    let moveSymbol = ""
+    let captureSymbol = ""
+    if(Object.keys(infoPossiblesMoves).length !== 0){
+        const isMoveSquare = infoPossiblesMoves.some(p => p.x === x && p.y === y)
+        if(isMoveSquare){
+            if(pieceName === "")
+                moveSymbol = "square-move-symbol" 
+            else
+                captureSymbol = "square-capture-symbol"
+        }
+    }
 
     return(
-        <div className="chess-square" onClickCapture={() => setLegalMoves(legalPieceMoves(x,y,pieceName,pieceColor))}>
-            <img src={pieceImg}/>
+        <div
+            className={`chess-square ${squareSelected} ${moveSymbol} ${captureSymbol}`}
+
+            onClick={() => clickCoordinates({x:x,y:y})}
+            >
+                {pieceName !== "" && 
+                    <div 
+                    className={`chess-piece ${pieceImg}`}
+                    > 
+                    </div>
+                }
+                
         </div>
     );
 }
